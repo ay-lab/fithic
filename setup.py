256fbcc10b01327e363b062b5ac3d932b5c9c51f
Arya <ferhatay@lji.org>
2017-07-31 18:40:42 -0700

    added setup.py for conda install

---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name="fithic",version='1.0.1', description='Hi-C Analysis software created and maintained by the Ay Lab', url='http://github.com/ay-lab/fithic', author='Ferhat Ay', author_email='ferhatay@lji.org', license='MIT', packages=['fithic'],install_requires=['scipy', 'numpy', 'matplotlib', 'scikit-learn'], zip_safe=False)
