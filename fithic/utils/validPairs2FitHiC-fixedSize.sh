2ad4753736d237db52d2d842987772e632b78837
Arya Kaul <akaul@herman-login3.hpc.lji.org>
2018-06-15 09:20:38 -0700

    Updated validPairs util

--- fithic/utils/validPairs2FitHiC-fixedSize.sh
+++ fithic/utils/validPairs2FitHiC-fixedSize.sh
@@ -15,7 +15,7 @@ libName=$2
 validPairsFile=$3
 echo "The resolution given is $w" 
 echo "Library name is $libName"
-outdir=.
+outdir=$4
 lowDistThres=$(echo "$w*2" | bc)
 echo "The lower distance threshold is $lowDistThres"
 
@@ -29,12 +29,12 @@ echo "The lower distance threshold is $lowDistThres"
     #compress
 ################################
 
-cat $validPairsFile | 
+zless $validPairsFile | 
 awk '{if(length($2)<=5 && length($5)<=5) {print $0}}' | grep -v chrM | 
 awk -v t=$lowDistThres '{if($2!=$5||($2==$5 && (sqrt(($3-$6)^2>t)))) {print $0}}' | 
 awk -v r=$w '{print $2, int($3/r)*r, $5, int($6/r)*r}' | 
 awk 'BEGIN{OFS="\t"} {if($1<$3 || ($1==$3 && $2<=$4)) {print $1, $2, $3, $4} else {print $3, $4, $1, $2}}' | 
 sort | uniq -c |  sed -r 's/^( *[^ ]+) +/\1\t/' | 
 awk -v r=$w 'BEGIN{OFS="\t"; FS="\t"} {print $2, $3+r/2, $4, $5+r/2, $1}' |
-gzip > $outdir/$libName\_fithic.interactionCounts.gz
+gzip > $outdir/$libName\_fithic.contactCounts.gz
 exit 
